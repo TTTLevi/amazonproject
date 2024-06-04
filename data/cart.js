0b17086baa555e88d153683b328bc411fe1d066a
Lev <thientong712001@gmail.com>
2024-06-05 06:51:29 +0700

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((CartItem) => {
+    if(productId === CartItem.productId) {
+      matchingItem = CartItem;
+    }
+  });
+
+  if(matchingItem){
+    matchingItem.quantity ++;
+  }
+  else{
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
