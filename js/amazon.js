e73d38f8fa6babac270c215c1183bbfada31f2bc
Lev <thientong712001@gmail.com>
2024-06-17 22:18:41 +0700

    update quantity link

--- js/amazon.js
+++ js/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -62,13 +62,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-
-    cart.forEach((CartItem) => {
-      cartQuantity += CartItem.quantity;
-    })
-
-    console.log(cartQuantity)
+  let cartQuantity = calculateCartQuantity();
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
